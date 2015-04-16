83772c128ab0ee857fc97043494bead1f6dc6907
hiroaki.murayama <hiroaki.murayama@mixi.co.jp>
2015-04-16 20:19:20 +0900

    Expect pickOdd( "ab"x50 ) == "a"x50

--- main.d
+++ main.d
@@ -36,5 +36,13 @@ unittest
 
 unittest
 {
+  import std.algorithm, std.range, std.array, std.stdio;
+  auto maxLenString   = repeat("ab").take(50).array.join("");
+  auto expectedString = repeat("a").take(50).array.join("");
+  assert(pickOdd(maxLenString) == expectedString);
+}
+
+unittest
+{
   assert(pickOdd("12 ") == "1 ");
 }
